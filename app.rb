eacf071207d3194d6346aa426a3c017b04114f61
Jesse Wenger <wengerj@gmail.com>
2021-04-07 15:20:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,11 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+<<<<<<< HEAD
   post '/food' do
    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end
+=======
+  <form method="POST" action="/food">
+>>>>>>> e5068e1ad75bcc50e34929732b5e40d9467a8537
 end
